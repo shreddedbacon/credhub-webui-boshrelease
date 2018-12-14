f228059cdde60377acdfb649a6d369f3d8b86a35
Ben Jackson <b@benjackson.email>
2018-12-14 23:39:05 +1100

    get ready for 0.0.2 of credhub-webui and include new flags

--- get_blobs.sh
+++ get_blobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Download blobs"
+VERSION=0.0.1
 if [ ! -d src/credhub-webui ]; then
   mkdir -p src/credhub-webui
 fi
-curl -L https://github.com/shreddedbacon/credhub-webui/releases/download/v0.0.1/credhub-webui-linux-0.0.1.tgz > src/credhub-webui/credhub-webui-linux-0.0.1.tgz
+curl -L "https://github.com/shreddedbacon/credhub-webui/releases/download/v$VERSION/credhub-webui-linux-$VERSION.tgz > src/credhub-webui/credhub-webui-linux-$VERSION.tgz"
