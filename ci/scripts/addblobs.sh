75d917042a9dec8d675a6b8687433b6a09b4af66
Ben Jackson <b@benjackson.email>
2019-02-21 19:59:01 +1100

    more pipeline testing

--- ci/scripts/addblobs.sh
+++ ci/scripts/addblobs.sh
@@ -12,6 +12,28 @@ then
   git config --global user.email "bacon@bot.local"
   git commit -am "Bump version(s) for $NEW_VERSION"
 fi
+
+export BOSH_CA_CERT=${BOSH_CA_CERT}
+export BOSH_CLIENT=${BOSH_USERNAME}
+export BOSH_CLIENT_SECRET=${BOSH_PASSWORD}
+
+bosh create-release --force
+bosh -n -e ${BOSH_TARGET} upload-release || echo "Continuing..."
+bosh -n -e ${BOSH_TARGET} -d ${BOSH_DEPLOYMENT} d ./manifests/deployment.yml \
+  -v credhub_webui_hostname=credhubwebui.local \
+  -v credhub_client_id=credhub-admin \
+  -v credhub_client_secret=${CREDHUB_ADMIN_SECRET} \
+  -v credhub_server=https://${BOSH_TARGET}:8844
+
+if [[ -n ${TEST_ERRAND} ]]; then
+  header "Running '${TEST_ERRAND}' validation errand"
+  bosh -n -e ${BOSH_TARGET} -d ${BOSH_DEPLOYMENT} run-errand ${TEST_ERRAND}
+fi
+
+header "Cleaning up..."
+bosh -n -e ${BOSH_TARGET} -d ${BOSH_DEPLOYMENT} deld --force || echo "continuing on..."
+bosh -n -e ${BOSH_TARGET} clean-up --client=${BOSH_USERNAME} || echo "continuing on..."
+
 popd
 
 cp -a bosh-release pushgit
